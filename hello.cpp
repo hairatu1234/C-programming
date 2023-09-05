d54534e049d2d337eeb921384273831d06ed7d94
Nagawa Hairatu <97951267+hairatu1234@users.noreply.github.com>
2023-09-05 18:38:20 +0300

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+// printing sentences on differnt lines
+    cout << "hello my name is hairatu \n";
+    cout << " I am old enough \n";
+    return 0;
+}
---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+     // use the code below if you do not include "using namespace std"
+    //std::cout << "hello World!"; 
+    cout << "hello World!";
+    return 0;
+}
